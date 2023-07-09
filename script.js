b8e8ed437a65316bbec301148974dd81884dd54d
McCue Nguyen <mccue.nguyen@gmail.com>
2023-07-09 16:02:10 -0700

    Set basic function for JS and finished html setup

--- script.js
+++ script.js
@@ -1,23 +1,50 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+$(document).ready(function () {
+
+  $('.saveBtn').on('click', function () {
+    var textVal = $(this).siblings('.description').val()
+    var hourId = $(this).parent().att('id')
+    localStorage.setItem(hourId, textVal)
+  })
+  localStorageReader()
+  timeColor()
+  today()
+
+
+  function localStorageReader() {
+    var timeBlocks = $('.container-lg').children()
+    var startingHour = timeBlocks.attr('id').split('-')[1]
+    var hourNumber =parseInt(startingHour)
+    for (var i = 0; i < 9; i++) {
+      var currentHour = hourNumber + i
+      var selectString = '#hour-${currentHour.toString()} .description'
+      var storageValue = localStorage.getItem('#hour-${currentHour.toString()}')
+      $(selectString).val(storageValue)
+    }
+  }
+
+  function timeColor() {
+    var timeBlocks = $('.container-lg').children()
+    var startingHour = timeBlocks.attr('id').split('-')[1]
+    var hourNumber = parseInt(startingHour)
+    var timeOfDay = dayjs().hour()
+    for (var i = 0; i < 9; i++){
+      var schedulerHour = hourNumber + i
+      var selectString = '#hour-${schedulerHour.toString()}'
+      if (schedulerHour < timeOfDay) {
+        $(selectString).addClass('past')
+      } else if (schedulerHour === timeOfDay){
+        $(selectString).addClass('present')
+      } else if (schedulerHour > timeOfDay){
+        $(selectString).addClass('future')
+      }
+    }
+  }
+
+
+  function today() {
+    var todayIs = dayjs().format('dddd MMM D, YYYY')
+    $('#currentDay').text("Current Date: " + todayIs)
+  }
+ 
+
 });
